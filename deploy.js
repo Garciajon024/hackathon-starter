3117aa27140aee70f7f2a9354e752d6e8ad60728
Jopnathan Garcia <garciajon024@gmail.com>
2021-09-11 21:25:01 -0500

    Updated to new instance

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.205.177.135',
+      host: '54.80.190.73',
       username: 'ubuntu',
       privateKey: 'labsuser.pem'
     })
